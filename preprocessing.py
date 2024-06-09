fe14171a80667a0bfebb415c06e8e2738a8920b4
soureshmondal <117149085+soureshmondal@users.noreply.github.com>
2024-06-09 16:07:29 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,15 +1,5 @@
 #!/usr/bin/env python3
-"""
-Preprocessing of PDF files.
-
-Converting PDF file to byte stream. Performing encoding with One Hot Encoding and n-grams.
-
-First example below is with the "dummy.pdf" file taken from
-https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf
-"""
-
 import glob
-# from sklearn.feature_extraction.text import CountVectorizer
 
 def get_file_byte_string(file):
     curr_file = open(file, "rb")
